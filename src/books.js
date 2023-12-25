a65d0ea6d41d4ce4a4575d683c3fdc8e178d88f8
Hadi Youssef <hmyoussef@gmail.com>
2023-12-25 16:45:30 -0500

    adding files

---
+++ src/books.js
@@ -0,0 +1,47 @@
+export const data = [
+    { id:1,
+      title: 'Forgive Me, Leonard Peacock',
+      author: 'Matthew Quick',
+      img: 'https://m.media-amazon.com/images/I/51wIBOX79UL._SY445_SX342_.jpg'
+    },    
+    { id:2,
+      title: 'Sky Raiders (Volume 1)',
+      author: 'Brandon Mull',
+      img: 'https://m.media-amazon.com/images/I/41d5yKX2s1L._SY445_SX342_.jpg'
+    },
+    { id:3,
+      title: 'Cracking the Coding Interview',
+      author: 'Gayle Laakmann McDowell',
+      img: 'https://m.media-amazon.com/images/I/410hiaPGyCL._SY445_SX342_.jpg'
+    },
+    { id:4,
+      title: 'The Road to React',
+      author: 'Robin Wieruch',
+      img: 'https://m.media-amazon.com/images/I/41P0Xp1jrNL._SX342_SY445_.jpg'
+    },
+    { id:5,
+      title: 'Dragon Ball Z Vol.4',
+      author: 'Akira Toriyama',
+      img: 'https://m.media-amazon.com/images/I/51p+LP1WJLL._SY445_SX342_.jpg'
+    },
+    { id:6,
+      title: 'Fluent Python: Clear, Concise, and Effective Programming',
+      author: 'Luciano Ramalho',
+      img: 'https://m.media-amazon.com/images/I/411qhFCwczL._SY445_SX342_.jpg'
+    },
+    { id:7,
+      title: 'Elden Ring: Official Art Book Volume I',
+      author: 'FromSoftware',
+      img: 'https://m.media-amazon.com/images/I/517ycY6AcCL._SY445_SX342_.jpg'
+    },
+    { id:8,
+      title: 'The University of Western Ontario',
+      author: 'John De Visser',
+      img: 'https://m.media-amazon.com/images/I/510-PmKHcVL._SX342_SY445_.jpg'
+    },
+    { id:9,
+      title: 'The Official Liverpool FC Annual 2023',
+      author: 'Liverpool FC',
+      img: 'https://m.media-amazon.com/images/I/610wLbvjuaL._SY445_SX342_.jpg'
+    },
+    ];
